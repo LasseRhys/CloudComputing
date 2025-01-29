b00808ad7382e42499500e426ecbe3cf5e690d6f
LasseRhys <56411103+LasseRhys@users.noreply.github.com>
2025-01-29 21:32:17 +0100

    Mergeconflict ?

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 terraform {
   required_providers {
     aws = {
@@ -17,4 +18,25 @@ provider "aws" {
   token = var.aws_token
   # Authentication requires the following environment variables:
   #     AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_SECRET_ACCESS_KEY
+=======
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.66.1"
+    }
+  }
+
+  required_version = ">= 1.5.5"
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  token = var.aws_token
+  # Authentication requires the following environment variables:
+  #     AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_SECRET_ACCESS_KEY
+>>>>>>> ea4b105eed05b09eb5aaadf78b4d5ffc738e6467
 }
