f477fd7b676be64350648551416c0e84e9a23c87
LasseRhys <selenfresser@gmx.de>
2026-02-04 20:00:08 +0100

    upidate

--- terraform/main.tf
+++ terraform/main.tf
@@ -363,7 +363,7 @@ resource "aws_lb_target_group" "web" {
 
   health_check {
     protocol = "HTTP"
-    path     = "/health"
+    path     = "/"
     interval = 30
     timeout  = 5
     healthy_threshold   = 3
